44d1a0d71c32350a056fc908f34be6fde1d299b2
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 20:07:35 +0200

    rebuild for osx with @loader_path-based install_name

--- csrc/hunspell/build-osx32.sh
+++ csrc/hunspell/build-osx32.sh
@@ -1,2 +1,2 @@
 PLATFORM=osx32 LIBNAME=libhunspell.dylib CXX=g++ \
-    CFLAGS="-arch i386 -mmacosx-version-min=10.4" ./build.sh
+    CFLAGS="-install_name @loader_path/libhunspell.dylib -arch i386 -mmacosx-version-min=10.4" ./build.sh
--- csrc/hunspell/build-osx64.sh
+++ csrc/hunspell/build-osx64.sh
@@ -1,2 +1,2 @@
 PLATFORM=osx64 LIBNAME=libhunspell.dylib CXX=g++ \
-    CFLAGS="-arch x86_64 -mmacosx-version-min=10.4" ./build.sh
+    CFLAGS="-install_name @loader_path/libhunspell.dylib -arch x86_64 -mmacosx-version-min=10.4" ./build.sh
